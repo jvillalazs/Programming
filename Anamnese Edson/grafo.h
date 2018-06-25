05f9facd07f73da9463703f5610dd070e3cbee72
manogray <lgmpf@icomp.ufam.edu.br>
2018-06-25 08:55:19 -0400

    correcao grafo.h

--- Anamnese Edson/grafo.h
+++ Anamnese Edson/grafo.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef GRAFO_H_
 #define GRAFO_H_
 
@@ -35,37 +34,3 @@ struct Grafo{
 
 TGrafo *criarGrafo(char nome[100], char sigla[50]);
 #endif /* GRAFO_H_ */
-=======
-#ifndef Grafo_H_
-#define Grafo_H_
-
-typedef struct Grafo TGrafo;
-
-//Métodos do Tipo TArrayDinamico
-typedef void (*TinserirGrafo)(TGrafo *vet,void* elem);
-typedef int (*TbuscarGrafo)(TGrafo *vet, void* valor);
-typedef void (*TDestroiGrafo) (TGrafo* vet);
-typedef struct Vertice TVertice { }
-
-//Dados do Tipo TArrayDinamico
-typedef struct dadoGrafo{
-	int vertices;
-	int arestas;
-	void* *;
-} TDadosADI;
-
-struct ArrayDinamico{
-	void *dadosADI; //Dados encapsulados
-	TinserirADI inserir;//Tipo Inserir
-	TinserirADIposicao inserirposicao; //Tipo insere na posicao
-	TremoverADI remover;//Tipo Remover
-	TbuscarADI buscar;//Tipo Buscar
-	TordenaADI ordenar; //Tipo Ordena
-	TDestroiADI destruir; //Tipo destroi
-};
-
-//Funcao construtor do TArrayDinamico
-TArrayDinamico *criarADI(int tamanhoInicial);
-#endif /* ADINAMICO_H_ */
-
->>>>>>> b8f0cf8a23a3dea63543409863f15c322ede4f6c
