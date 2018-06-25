da453e74e4c1b894fe6060a8bb379a33a394dda9
manogray <lgmpf@icomp.ufam.edu.br>
2018-06-25 08:51:47 -0400

    add perguntas, estrutura grafo

---
+++ Anamnese Edson/grafo.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "adinamico.h"
+#include "grafo.h"
+
+static TDadosGrafo *criarDadosGrafo(char pergunta[400], char resposta[200], int pontuacao){
+	TDadosGrafo *aloca = malloc(sizeof(TDadosGrafo));
+	if(aloca!= NULL){
+		
+	}
+	return aloca;
+}
+
+
+static void inserirADI(TArrayDinamico *vet, void* elem){
+	TDadosADI *dadosVet = vet->dadosADI;
+
+    //Verifica se o vetor está cheio, para redimensioná-lo caso precise
+	if (dadosVet->ocupacao == dadosVet->tamanho){
+		dadosVet->vetor = realloc(dadosVet->vetor,sizeof(void*)*(2*dadosVet->tamanho));
+		dadosVet->tamanho = 2*dadosVet->tamanho;
+	}
+
+	dadosVet->vetor[dadosVet->ocupacao] = elem;
+	dadosVet->ocupacao += 1;
+}
+
+static void inserirADIposicao(TArrayDinamico* vet, void* elem, int posicao){
+	TDadosADI* dadoVet = vet->dadosADI;
+
+	if(dadoVet->vetor[posicao] == NULL){
+		dadoVet->vetor[posicao] = elem;
+		dadoVet->ocupacao += 1;
+	}else{
+		printf("\n\nHorario ocupado. Digite outro por favor: ");
+		setbuf(stdin,NULL);
+		scanf("%d",&posicao);
+		inserirADIposicao(vet,elem,posicao);
+	}
+}
+
+static int buscarADI(TArrayDinamico *vet, void *elem){
+	TDadosADI *dadosVet = vet->dadosADI;
+	TComparavel *compara = elem;
+
+	int indice=0;
+	while ((indice<dadosVet->ocupacao) && (compara->compara(elem,dadosVet->vetor[indice])!=0) )
+		indice++;
+
+	return (indice==dadosVet->ocupacao?-1:indice);
+
+}
+
+static void* removerADI(TArrayDinamico *vet, void *procurado){
+	TDadosADI *dadosVet = vet->dadosADI;
+
+	int indice=buscarADI(vet,procurado);
+	void *elem = NULL;
+	if (indice!=-1){
+		elem = dadosVet->vetor[indice];
+		for(;indice<dadosVet->ocupacao;indice++)
+			dadosVet->vetor[indice]=dadosVet->vetor[indice+1];
+		dadosVet->ocupacao--;
+	}
+
+	return elem;
+}
+
+static void DestroiADI(TArrayDinamico* vet) {
+    vet->ordenar = NULL;
+    vet->inserir = NULL;
+    vet ->buscar = NULL;
+    vet->remover = NULL;
+    vet->dadosADI = NULL;
+    free(vet);
+}
+
+
+TArrayDinamico *criarADI(int tamanhoInicial){
+	TArrayDinamico *adi = malloc(sizeof(TArrayDinamico));
+	if(adi != NULL){
+		adi->dadosADI = criarDadosADI(tamanhoInicial);
+		adi->inserir = inserirADI;
+		adi->inserirposicao = inserirADIposicao;
+		adi->buscar = buscarADI;
+		adi->remover=removerADI;
+		adi->destruir=DestroiADI;
+	}
+	return adi;
+}
---
+++ Anamnese Edson/grafo.h
@@ -0,0 +1,36 @@
+#ifndef GRAFO_H_
+#define GRAFO_H_
+
+typedef struct Grafo TGrafo;
+
+typedef void (*TinserirGrafo)(TGrafo *graf,void* elem);
+typedef int (*TbuscarGrafo)(TGrafo *graf, void* param1, void* param2);
+typedef void (*TDestroiGrafo) (TGrafo* graf);
+
+typedef struct Tvertice{
+	char pergunta[400];
+	char resposta[200];
+	int pontuacao;
+	int visitado;
+	int marcado;
+	Tvertice* coleguinha;
+} Tvertice;
+
+typedef struct dadosGrafo{
+	char nome[100];
+	char sigla[50];
+	int vertices;
+	int visitados;
+	int marcados;
+	Tvertice fonte;
+} TDadosGrafo;
+
+struct Grafo{
+	void *dadosGrafo;
+	TinserirGrafo inserir;
+	TbuscarGrafo buscar;
+	TDestroiGrafo destruir;
+};
+
+TGrafo *criarGrafo(char nome[100], char sigla[50]);
+#endif /* GRAFO_H_ */
