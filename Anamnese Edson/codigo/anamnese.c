b8d182423481f4ea6f3e4d64cf4819a5dfc72911
Unknown <lgmpf@icomp.ufam.edu.br>
2018-06-29 08:43:25 -0400

    validacao resposta

--- Anamnese Edson/codigo/anamnese.c
+++ Anamnese Edson/codigo/anamnese.c
@@ -27,21 +27,41 @@ TArrayDinamico* getPerguntas(char *diretorio){
 void ColetaRespostas(TArrayDinamico* vet){
     TDadosADI* dadosVet = vet->dadosADI;
     int contador = 0;
+    int temp;
+    int ok;
     while(contador < dadosVet->ocupacao){
+        ok = 0;
         TPergunta* ppp = dadosVet->vetor[contador];
         TDadosPergunta* dadosPPP = ppp->dadosPergunta;
         printf("\n\n%d) %s\n\n",contador+1,dadosPPP->perguntinha);
         printf("[1]Muitissimo   [2]Muito   [3]Pouco   [0]Nao se aplica\n");
-        scanf("%d",&dadosPPP->resposta);
+        while(ok == 0){
+            scanf("%d",&temp);
+            if(temp <= 3 || temp >= 0){
+                dadosPPP->resposta = temp;
+                ok = 1;
+            }else{
+                printf("\n\nresposta invalida!");
+            }
+        }
         contador = contador + 1;
     }
 }
+
+
 
 int main(){
 
     TArrayDinamico* Perguntas = getPerguntas("../../Perguntas.txt");
 
     ColetaRespostas(Perguntas);
+
+    TDadosADI* dadosPerguntas = Perguntas->dadosADI;
+    TPergunta* per = dadosPerguntas->vetor[0];
+    TDadosPergunta* dadosPer = per->dadosPergunta;
+
+    printf("%s",dadosPer->perguntinha);
+    printf(" - %d",dadosPer->resposta);
 
     return 0;
 }
